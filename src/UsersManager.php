62344413a9855921e57a74cedb4ebbe5ccb7bb7a
Orest Budzhak <doom4eg@gmail.com>
2014-11-25 13:00:07 +0200

    PasswordTooShortException remove

--- src/UsersManager.php
+++ src/UsersManager.php
@@ -72,9 +72,9 @@ class UsersManager
 
     public function login($siteId, $email, $password)
     {
-        if (strlen($password) < 6) {
-            //throw new PasswordTooShortException("User password is too short!");
-        }
+//        if (strlen($password) < 6) {
+//            throw new PasswordTooShortException("User password is too short!");
+//        }
 
         $data = $this->getUserData($siteId, $email);
 
