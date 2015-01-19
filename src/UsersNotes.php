2fa4a43832c7feccfd5b98eb324dfa4ba40e96fe
Gary <andrey@dataedu.com>
2015-01-19 17:51:40 +0200

    auth log countries

--- src/UsersNotes.php
+++ src/UsersNotes.php
@@ -57,14 +57,12 @@ class UsersNotes {
         $userId = (int)$userId;
         $records = $this->db->query("
             select SQL_CALC_FOUND_ROWS 
-                #*, p.type payment_type,
-                
                 l.date date,
                 admin.email actor,
                 l.type type,
                 l.content description,
                 
-                auth.ip, auth.mobile, auth.tablet, auth.browser, auth.browser_version, auth.platform, auth.platform_version
+                auth.ip, auth.mobile, auth.tablet, auth.browser, auth.browser_version, auth.platform, auth.platform_version, auth.country
             
             from users_system_notes l
             
@@ -72,15 +70,9 @@ class UsersNotes {
             
             left join users_auth_log auth on l.`type` = 'auth' and l.join_id = auth.id and l.user_id = auth.user_id
             
-            #left join orders o on l.`type` = 'pay' and l.user_id = o.user_id
-            #left join orders_payments p on l.`type` = 'pay' and l.join_id = p.id and p.order_id = o.id
-            #left join orders_payments_products pp on pp.order_payment_id = p.id
-            #left join orders_products op on pp.order_product_id = op.id
-            #left join products pr on op.product_id = pr.id
-            
             where l.user_id = {$userId}
             group by l.id
-            order by date desc  " . $limit)->fetchAll(PDO::FETCH_ASSOC);
+            order by date desc " . $limit)->fetchAll(PDO::FETCH_ASSOC);
 
         $total = $this->db->query("SELECT FOUND_ROWS()")->fetchColumn();
 
