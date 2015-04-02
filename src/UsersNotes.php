e7ade40e5f106a8f990307c83ccad41cbb13f8c8
Orest Budzhak <doom4eg@gmail.com>
2015-04-02 16:47:49 +0300

    add methods for every type of notes

--- src/UsersNotes.php
+++ src/UsersNotes.php
@@ -2,34 +2,218 @@
 
 namespace CS\Users;
 
-use \PDO,
-    \Exception;
+use PDO,
+    Exception,
+    CS\Models\User\UsersSystemNotes\UsersSystemNoteRecord;
 
-class UsersNotes {
+class UsersNotes
+{
 
     const TYPE_SYSTEM = 'sys';
-    const TYPE_AUTH  = 'auth';
+    const TYPE_AUTH = 'auth';
 
+    /**
+     *
+     * @var PDO
+     */
+    private $db;
+
+    /**
+     *
+     * @var int
+     */
+    private $adminId;
     protected $availableTypes = array(
         self::TYPE_AUTH,
         self::TYPE_SYSTEM,
     );
 
-    public function __construct(PDO $db)
+    public function __construct(PDO $db, $adminId = null)
     {
         $this->db = $db;
+        $this->adminId = $adminId;
+    }
+
+    public function deviceAdded($userId, $deviceId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("New device #{$deviceId} added")
+                ->save();
+    }
+
+    public function deviceDeletedFromCp($userId, $deviceId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Device #{$deviceId} deleted from CP");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function licenseAssigned($userId, $licenseId, $deviceId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Subscription #{$licenseId} assigned to device #{$deviceId}");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function licenseAdded($userId, $licenseId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Subscription #{$licenseId} added");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function licenseExpired($userId, $licenseId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Subscription #{$licenseId} expired");
+    }
+
+    public function licenseDropped($userId, $licenseId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Subscription #{$licenseId} expired");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function licenseUnAssigned($userId, $licenseId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Subscription #{$licenseId} unassigned");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function deviceLimitsUpdated($userId, $deviceId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Device #{$deviceId} limits updated");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function accountEntered($userId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Login under account");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function accountLocked($userId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Account locked");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function accountUnlocked($userId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Account unlocked");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function accountRestored($userId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Restore email successfully sent!");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
+    }
+
+    public function accountCustomPasswordSaved($userId)
+    {
+        $usersSystemNote = new UsersSystemNoteRecord($this->db);
+        $usersSystemNote->setType(UsersSystemNoteRecord::TYPE_SYSTEM)
+                ->setUserId($userId)
+                ->setContent("Custom password successfully saved!");
+
+        if ($this->adminId !== null) {
+            $usersSystemNote->setAdminId($this->adminId);
+        }
+
+        $usersSystemNote->save();
     }
 
     public function addSystemNote($userId, $type = self::TYPE_SYSTEM, $adminId = null, $joinId = null, $content = '')
     {
-        switch(true){
-            case !in_array($type, $this->availableTypes):
+        switch (true) {
+            case!in_array($type, $this->availableTypes):
                 throw new WrongSystemNoteType;
 
             case $type == self::TYPE_SYSTEM && !is_null($joinId):
-            case $type != self::TYPE_AUTH && (int)$joinId:
+            case $type != self::TYPE_AUTH && (int) $joinId:
                 throw new WrongSystemNoteParams;
-
         }
 
         $query = $this->db->prepare("
@@ -48,13 +232,14 @@ class UsersNotes {
         return $query->execute();
     }
 
-    public function getSystemNotes($userId, $params = array()) {
+    public function getSystemNotes($userId, $params = array())
+    {
         $limit = "";
         if (isset($params['iDisplayStart'])) {
             $limit = "LIMIT " . intval($params['iDisplayStart']) . ", " . intval($params['iDisplayLength']);
         }
 
-        $userId = (int)$userId;
+        $userId = (int) $userId;
         $records = $this->db->query("
             select SQL_CALC_FOUND_ROWS 
                 unix_timestamp(l.date) timestamp,
@@ -84,9 +269,14 @@ class UsersNotes {
         );
     }
 
-
 }
 
-class WrongSystemNoteType extends Exception {}
+class WrongSystemNoteType extends Exception
+{
+    
+}
 
-class WrongSystemNoteParams extends Exception {}
+class WrongSystemNoteParams extends Exception
+{
+    
+}
